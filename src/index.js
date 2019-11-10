6c6492073f444432669af139f07fe8b2c3eccc23
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2019-11-10 11:10:18 -0600

    index: Add time slider

--- src/index.js
+++ src/index.js
@@ -2,19 +2,27 @@ import * as d3 from "d3";
 import $ from 'jquery';
 import _ from 'lodash';
 window.$ = window.jQuery = $;
-require('semantic-ui-css/semantic');
-require('semantic-ui-css/semantic.min.css');
+require('fomantic-ui-css/semantic');
+require('fomantic-ui-css/semantic.min.css');
 
 let patientIDs = new Set();
-let timePeriods = new Set();
+let timePeriods = [];
 
 window.addEventListener('DOMContentLoaded', () => {
   d3.csv('/data/datasets/symptoms_period.csv').then((data) => {
     const selectEl = d3.select('#patient-list');
-    timePeriods = new Set(
-      data.map(({ period }) => parseInt(period, 10))
-        .sort((a, b) => a - b)
-    );
+    timePeriods = _.sortedUniq(data.map(({ period }) => parseInt(period, 10))
+      .sort((a, b) => a - b));
+    $('#time-slider').slider({
+      min: Math.min(...timePeriods),
+      max: 6 * (Math.floor(Math.max(...timePeriods) / 6) + 1),
+      step: 6,
+    });
+    d3.select('#time-slider ul li:last-child')
+      .text('> 2 years');
+    d3.select('#time-slider ul li:first-child')
+      .text('Baseline');
+
     patientIDs = new Set(
       data.map(({ patientId }) => parseInt(patientId, 10))
         .sort((a, b) => a - b)
