ae710d3def2dd6e4a2faae83597cabac1af4762c
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2019-11-10 10:35:28 -0600

    index.js: Use correct patientIDs

--- src/index.js
+++ src/index.js
@@ -5,12 +5,24 @@ window.$ = window.jQuery = $;
 require('semantic-ui-css/semantic');
 require('semantic-ui-css/semantic.min.css');
 
+let patientIDs = new Set();
+let timePeriods = new Set();
+
 window.addEventListener('DOMContentLoaded', () => {
   d3.csv('/data/datasets/symptoms_period.csv').then((data) => {
-    _.sortedUniq(data.map(({ patientId }) => parseInt(patientId, 10)))
-      .forEach((id) => {
-        d3.select('#patient-list').append('option').text(id).attr('value', id);
-      });
+    const selectEl = d3.select('#patient-list');
+    timePeriods = new Set(
+      data.map(({ period }) => parseInt(period, 10))
+        .sort((a, b) => a - b)
+    );
+    patientIDs = new Set(
+      data.map(({ patientId }) => parseInt(patientId, 10))
+        .sort((a, b) => a - b)
+    );
+    patientIDs.forEach((id) => {
+      selectEl.append('option').text(id).attr('value', id);
+    });
   });
+
   $('#patient-list').dropdown({ maxSelections: 3 });
 });
