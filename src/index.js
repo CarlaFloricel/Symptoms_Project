4fdcfbcedbc1c1f321c46f21015a1f0a569c696e
Carla Floricel <carla.floricel@gmail.com>
2019-11-10 17:36:55 -0600

    prototype photos addes, screen split in 4

--- src/index.js
+++ src/index.js
@@ -34,3 +34,17 @@ window.addEventListener('DOMContentLoaded', () => {
 
   $('#patient-list').dropdown({ maxSelections: 3 });
 });
+
+// $('.tablinks').click((evt, view)=> {
+//   var i, tabcontent, tablinks;
+//   tabcontent = document.getElementsByClassName("tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
+//   tablinks = document.getElementsByClassName("tablinks");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
+//   document.getElementById(view).style.display = "block";
+//   evt.currentTarget.className += " active";
+// });
