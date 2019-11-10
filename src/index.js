af76bcfdf5a1f02eb91ac6eb69751e9f27829a12
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2019-11-10 10:12:30 -0600

    index: Update patient ID dropdown

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import * as d3 from "d3";
 import $ from 'jquery';
+import _ from 'lodash';
 window.$ = window.jQuery = $;
 require('semantic-ui-css/semantic');
 require('semantic-ui-css/semantic.min.css');
 
 window.addEventListener('DOMContentLoaded', () => {
-  $('.ui.dropdown').dropdown({maxSelections: 3});
+  d3.csv('/data/datasets/symptoms_period.csv').then((data) => {
+    _.sortedUniq(data.map(({ patientId }) => parseInt(patientId, 10)))
+      .forEach((id) => {
+        d3.select('#patient-list').append('option').text(id).attr('value', id);
+      });
+  });
+  $('#patient-list').dropdown({ maxSelections: 3 });
 });
