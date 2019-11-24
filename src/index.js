d2d36627660e107d53a9000f994f6bfac07ad6d9
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2019-11-23 23:10:04 -0600

    scatterplot: Use circle packing layout

--- src/index.js
+++ src/index.js
@@ -4,114 +4,26 @@ import _ from 'lodash';
 window.$ = window.jQuery = $;
 require('fomantic-ui-css/semantic');
 require('fomantic-ui-css/semantic.min.css');
-import innerJoin from 'lodash-joins/lib/hash/hashInnerJoin';
 
 import TimeSlider from './timeslider';
 import ScatterPlot from './scatterplot';
 
-window.addEventListener('DOMContentLoaded', () => {
-  d3.csv('/data/datasets/symptoms_period.csv').then((data) => {
-    const timePeriods = _.sortedUniq(data.map(({ period }) => parseInt(period, 10))
-      .sort((a, b) => a - b));
-    const timeSlider = new TimeSlider('#time-slider', timePeriods, drawScatterPlots);
-    timeSlider.init();
-  });
+let scatterPlot = null;
 
-  $('#patient-list').dropdown({ maxSelections: 3 });
-  $('#scatterplot-legend').hide();
-
-  // Connecting tab event listeners
-  $('#scatterplot-btn').on('click', function () {
-    $('#scatterplot-btn').toggleClass('active');
-    $('#correlation-btn').toggleClass('active');
-    $('#scatterplot').show();
-    $('#scatterplot-legend').show();
-    $('#star-plot').hide();
-    $('#matrix').hide();
-  });
-  $('#correlation-btn').on('click', function () {
-    $('#scatterplot-btn').toggleClass('active');
-    $('#correlation-btn').toggleClass('active');
-    $('#scatterplot').hide();
-    $('#scatterplot-legend').hide();
-    $('#matrix').show();
-  });
-  $('#mult-symptoms-btn').on('click', function () {
-    $('#mult-symptoms-btn').toggleClass('active');
-    $('#mult-patients-btn').toggleClass('active');
-    $('#tendril').hide();
-    $('#stack').show();
-    $('#star-plot').hide();
-  });
-  $('#mult-patients-btn').on('click', function () {
-    $('#mult-symptoms-btn').toggleClass('active');
-    $('#mult-patients-btn').toggleClass('active');
-    $('#tendril').show();
-    $('#star-plot').hide();
-    $('#stack').hide();
-  $('#mult-timestamps-btn').on('click', function () {
-    $('#mult-symptoms-btn').toggleClass('active');
-    $('#mult-patients-btn').toggleClass('active');
-    $('#star-plot').show();
-    $('#tendril').hide();
-    $('#stack').hide();
-  });
-  });
-});
-
-const scatterPlotLevels = ['Mild', 'Average', 'Severe'];
-const scatterPlots = { 0: null, 1: null, 2: null };
-
-async function drawScatterPlots(period) {
+async function loadDataset(period) {
   const patients = await d3.csv('/data/datasets/patients_complete.csv');
   const clusters = await d3.csv(`/data/output/raw_result-time-${period}.csv`);
-  window.patients = patients;
-  window.clusters = clusters;
-  const merged = innerJoin(clusters, (x) => x['patientId'], patients, (x) => x['patientId']);
-  const x = d3.extent(merged.map(m => parseInt(m['age'])));
-  const y = d3.extent(merged.map(m => parseFloat(m['total_dose'])));
-  const domain = {
-    x: [x[0] - 10, x[1] + 10],
-    y: [y[0] - 1, y[1] + 1],
-  };
-
-  Object.values(scatterPlots).forEach(plot => plot ? plot.clear() : null);
-
-  const patientIds = merged.map(({ patientId }) => parseInt(patientId));
-  const clusterIds = new Set(merged.map(({ cluster }) => parseInt(cluster)));
-  clusterIds.forEach((clusterId) => {
-    const data = merged.filter((r) => parseInt(r.cluster) === clusterId)
-      .map((row) => {
-        return {
-          age: parseInt(row['age']),
-          dosage: parseFloat(row['total_dose']),
-          gender: row['gender'],
-          tumorCategory: row['t_category'],
-        };
-      })
-      .filter(d => d);
-    let plot = scatterPlots[clusterId];
-    if (!scatterPlots[clusterId]) {
-      const margin = { left: 10, right: 20, top: 30, bottom: 30 };
-      const width = 400;
-      const height = 300;
-      plot = new ScatterPlot(
-        '#scatterplot',
-        scatterPlotLevels[clusterId],
-        margin,
-        width,
-        height,
-        data,
-        domain);
-      plot.init();
-    } else {
-      plot.update(data);
-    }
-    scatterPlots[clusterId] = plot;
-  });
-
-  $('#scatterplot-legend').show();
-  updatePatientIds(new Set(patientIds));
+  const data = clusters
+    .filter(cluster => patients.find(patient => patient.patientId === cluster.patientId))
+    .map(cluster => ({ ...cluster, ...patients.find(patient => patient.patientId === cluster.patientId) }))
+    .sort((a, b) => b.cluster - a.cluster)
+    .map(({ cluster, gender, patientId, t_category }) => ({
+      cluster: parseInt(cluster),
+      patientId,
+      gender,
+      t_category,
+    }));
+  return data;
 }
 
 async function updatePatientIds(ids) {
@@ -124,3 +36,77 @@ async function updatePatientIds(ids) {
     selectEl.append(optionEl);
   })
 }
+
+async function drawClusters(period) {
+  const data = await loadDataset(period);
+  const patientIds = data.map(({ patientId }) => parseInt(patientId));
+
+  if (!scatterPlot && data.length > 0) {
+    scatterPlot = new ScatterPlot('#scatterplot', 512, 512, data);
+    scatterPlot.init();
+  } else if (scatterPlot) {
+    scatterPlot.clear();
+    scatterPlot.update(data);
+  }
+
+  // if ($('#scatterplot').is(":visible"))
+  //   $('#scatterplot-legend').show();
+
+  updatePatientIds(new Set(patientIds));
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  d3.csv('/data/datasets/symptoms_period.csv').then((data) => {
+    const timePeriods = _.sortedUniq(data.map(({ period }) => parseInt(period, 10))
+      .sort((a, b) => a - b));
+    const timeSlider = new TimeSlider('#time-slider', timePeriods, drawClusters);
+    timeSlider.init();
+  });
+
+  $('#patient-list').dropdown({ maxSelections: 3 });
+  $('#scatterplot-legend').hide();
+
+  // Connecting tab event listeners
+  $('#scatterplot-btn').on('click', function () {
+    $('#scatterplot-btn').toggleClass('active');
+    $('#correlation-btn').toggleClass('active');
+    $('#scatterplot').show();
+    // $('#scatterplot-legend').show();
+    $('#star-plot').hide();
+    $('#matrix').hide();
+  });
+
+  $('#correlation-btn').on('click', function () {
+    $('#scatterplot-btn').toggleClass('active');
+    $('#correlation-btn').toggleClass('active');
+    $('#scatterplot').hide();
+    // $('#scatterplot-legend').hide();
+    $('#matrix').show();
+  });
+
+  $('#mult-symptoms-btn').on('click', function () {
+    $('#mult-symptoms-btn').toggleClass('active');
+    $('#mult-patients-btn').toggleClass('active');
+    $('#tendril').hide();
+    $('#stack').show();
+    $('#star-plot').hide();
+  });
+
+  $('#mult-patients-btn').on('click', function () {
+    $('#mult-symptoms-btn').toggleClass('active');
+    $('#mult-patients-btn').toggleClass('active');
+    $('#tendril').show();
+    $('#star-plot').hide();
+    $('#stack').hide();
+  });
+
+  $('#mult-timestamps-btn').on('click', function () {
+    $('#mult-symptoms-btn').toggleClass('active');
+    $('#mult-patients-btn').toggleClass('active');
+    $('#star-plot').show();
+    $('#tendril').hide();
+    $('#stack').hide();
+  });
+
+  drawClusters();
+});
--- src/scatterplot.js
+++ src/scatterplot.js
@@ -1,95 +1,100 @@
 import * as d3 from 'd3';
 
 class ScatterPlot {
-  constructor(selector, label, margin, width, height, data, domain) {
+  drawHexagon(data) {
+    return d3.line()
+      .x(function (d) { return d.x; })
+      .y(function (d) { return d.y; })
+      .curve(d3.curveCardinalClosed.tension(0.5))(data);
+  }
+
+  constructor(selector, width, height, data) {
     this.selector = selector;
-    this.margin = margin;
     this.width = width;
     this.height = height;
     this.data = data;
-    this.label = label;
-    this.domain = domain;
+  }
+
+  pack(data) {
+    const { width, height } = this;
+    const packer = d3.pack().size([width - 2, height - 2]).padding(3);
+    return packer(d3.hierarchy({ children: data }).sum(d => d.cluster + 1));
   }
 
   init() {
-    const { data } = this;
+    const { data, width, height } = this;
+    const root = this.pack(data);
+
+    this.colorScale = d3.scaleOrdinal()
+      .domain(d3.extent(data.map(d => d.cluster)))
+      .range(['red', 'orange', 'green'])
+
     this.svg = d3.select(this.selector)
       .append('svg')
-      .attr('id', this.label)
-      .attr('class', 'plot')
-      .attr('viewBox', `0 0 ${this.width + this.margin.left + this.margin.right} ${this.height + this.margin.top + this.margin.bottom}`)
-    // .attr('width', this.width + this.margin.left + this.margin.right)
-    // .attr('height', this.height + this.margin.top + this.margin.bottom);
-    const g = this.svg.append('g')
-      .attr('transform', `translate(${this.margin.left},${this.margin.top})`);
+      .attr('width', width)
+      .attr('height', height)
+      .attr("viewBox", `0 0 ${width}, ${height}`)
+      .attr("font-size", 10)
+      .attr("font-family", "sans-serif")
+      .attr("text-anchor", "middle")
+      .attr('preserveAspectRatio', "xMidYMid meet");
 
-    const leftStart = this.margin.left + 45;
+    this.drawLeaves(root.leaves());
+  }
 
-    g.append('text')
-      .attr('class', 'plot-title')
-      .text(this.label);
+  drawLeaves(leaves) {
+    const { svg } = this;
+    const leaf = svg.selectAll("g")
+      .data(leaves)
+      .join("g")
+      .attr("transform", d => `translate(${d.x + 1},${d.y + 1})`)
+      .classed("leaf", true);
 
-    this.xScale = d3.scaleLinear()
-      .domain(this.domain.x)
-      .range([leftStart, this.width - this.margin.right]);
-    this.yScale = d3.scaleLinear()
-      .domain(this.domain.y)
-      .range([this.height - this.margin.bottom, this.margin.top]);
-    this.sizeScale = d3.scaleOrdinal()
-      .domain(d3.extent(data.map(d => d.tumorCategory)))
-      .range([4, 8, 12, 16]);
-    this.colorScale = d3.scaleOrdinal()
-      .domain(['Male', 'Female'])
-      .range(['rgba(33, 127, 242, 0.52)', 'rgba(242, 33, 166, 0.52)']);
+    leaf.append("path")
+      .attr("d", d => {
+        const h = (Math.sqrt(3) / 2),
+          radius = d.r,
+          xp = 0,
+          yp = 0,
+          hexagonData = [
+            { "x": radius + xp, "y": yp },
+            { "x": radius / 2 + xp, "y": radius * h + yp },
+            { "x": -radius / 2 + xp, "y": radius * h + yp },
+            { "x": -radius + xp, "y": yp },
+            { "x": -radius / 2 + xp, "y": -radius * h + yp },
+            { "x": radius / 2 + xp, "y": -radius * h + yp }
+          ];
+        return this.drawHexagon(hexagonData);
+      })
+      .attr("id", d => (d.leafUid = `leaf-${d.data.patientId}`))
+      .attr("fill", d => this.colorScale(d.data.cluster))
+      .attr("stroke", d => this.colorScale(d.data.cluster))
+      .attr("stroke-opacity", d => d.data.gender === 'Female' ? 0 : 1.0)
+      .attr("fill-opacity", d => d.data.gender === 'Female' ? 0 : 0.5);
 
-    g.selectAll('circle')
-      .data(data)
-      .enter()
-      .append('circle')
-      .attr('cx', d => this.xScale(d.age))
-      .attr('cy', d => this.yScale(d.dosage))
-      .attr('r', d => this.sizeScale(d.tumorCategory))
-      .style('fill', d => this.colorScale(d.gender));
+    leaf.append("circle")
+      .attr("id", d => (d.leafUid = `leaf-${d.data.patientId}`))
+      .attr("r", d => d.r)
+      .attr("fill", d => this.colorScale(d.data.cluster))
+      .attr("stroke", d => this.colorScale(d.data.cluster))
+      .attr("stroke-opacity", d => d.data.gender === 'Male' ? 0 : 1.0)
+      .attr("fill-opacity", d => d.data.gender === 'Male' ? 0 : 0.5);
 
-    g.append('g')
-      .attr('class', 'axis')
-      .attr('transform', `translate(${0},${this.height - this.margin.top})`)
-      .call(d3.axisBottom(this.xScale));
+    leaf.append("text")
+      .text(d => d.data.patientId);
 
-    g.append('g')
-      .attr('class', 'axis')
-      .attr('transform', `translate(${leftStart},0)`)
-      .call(d3.axisLeft(this.yScale));
-
-    g.append('text')
-      .attr('transform', `translate(${this.width / 2},${this.height})`)
-      .style('text-anchor', 'middle')
-      .text('Age');
-
-    g.append('text')
-      .attr('transform', 'rotate(-90)')
-      .attr('y', 0 - this.margin.left + 16)
-      .attr('x', 0 - (this.height / 2))
-      .attr('dy', '1em')
-      .style('text-anchor', 'middle')
-      .text('Total Dosage');
+    leaf.append("title")
+      .text(d => d.data.patientId);
   }
 
   clear() {
-    this.svg.selectAll('circle').remove();
+    this.svg.selectAll('.leaf').remove();
   }
 
   update(data) {
-    const circles = this.svg.select('g')
-      .selectAll('circle')
-      .data(data);
-
-    circles.enter()
-      .append('circle')
-      .attr('cx', d => this.xScale(d.age))
-      .attr('cy', d => this.yScale(d.dosage))
-      .attr('r', d => this.sizeScale(d.tumorCategory))
-      .style('fill', d => this.colorScale(d.gender));
+    this.data = data;
+    const leaves = this.pack(data).leaves();
+    this.drawLeaves(leaves);
   }
 }
 
